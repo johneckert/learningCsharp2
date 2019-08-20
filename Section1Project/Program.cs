3c364704c6895849c10046de4f760bd570e16d97
johneckert <johnteckert@gmail.com>
2019-08-20 16:42:39 -0400

    Section 2 excercise done

--- Section1Project/Program.cs
+++ Section1Project/Program.cs
@@ -3,106 +3,139 @@ using System.Collections.Generic;
 
 namespace Section1Project
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      var Students = new List<Student>();
+      var Teachers = new List<Teacher>();
+      var cont = true;
+
+      while (cont)
+      {
+        Console.WriteLine("Are you a teacher? y/n");
+        if (Console.ReadLine() != "y")
         {
-            var Students = new List<Student>();
+          var newStudent = new Student();
+          newStudent.Name = Util.Console.Ask("Name:");
+          newStudent.Grade = int.Parse(Util.Console.Ask("Grade:"));
+          newStudent.Birthday = Util.Console.Ask("DOB:");
+          newStudent.Address = Util.Console.Ask("Address:");
+          newStudent.Phone = int.Parse(Util.Console.Ask("Phone:"));
+          Students.Add(newStudent);
 
-            var cont = true;
-            
-            while (cont) {
-                var newStudent = new Student();
-                
-                newStudent.Name = Util.Console.Ask("Name:");
-
-                newStudent.Grade = int.Parse(Util.Console.Ask("Grade:"));
-
-                newStudent.Birthday = Util.Console.Ask("DOB:");
-
-                newStudent.Address = Util.Console.Ask("Address:");
-
-                newStudent.Phone = int.Parse(Util.Console.Ask("Phone:"));
-
-                Students.Add(newStudent);
-
-                Console.WriteLine("Add another Student? (y/n)");
-                if (Console.ReadLine() != "y") {
-                    cont = false;
-                };
-            } 
-
-            foreach (var student in Students) {
-                Console.WriteLine($"Name: {student.Name}, Grade: {student.Grade}, Birthday: {student.Birthday}, Address: {student.Address}, Phone: {student.getPhone()}.");
-            }
-            Console.WriteLine($"{Student.Count} students.");
         }
+        else
+        {
+          var newTeacher = new Teacher();
+          newTeacher.Name = Util.Console.Ask("Name:");
+          newTeacher.Subject = Util.Console.Ask("Subject:");
+          newTeacher.Address = Util.Console.Ask("Address:");
+          newTeacher.Phone = int.Parse(Util.Console.Ask("Phone:"));
+          Teachers.Add(newTeacher);
+        }
+
+        Console.WriteLine("Add another Person? (y/n)");
+        if (Console.ReadLine() != "y")
+        {
+          cont = false;
+        };
+      }
+
+      foreach (var student in Students)
+      {
+        student.Display();
+      }
+      Console.WriteLine($"There are {Student.Count} students.");
+
+      foreach (var teacher in Teachers)
+      {
+        teacher.Display();
+      }
+      Console.WriteLine($"There are {Teacher.Count} teachers.");
+    }
+  }
+
+  class Member
+  {
+    public string Name;
+    public string Address;
+    protected int phone;
+  }
+  class Student : Member
+  {
+    static public int Count;
+    public int Grade;
+    public string Birthday;
+
+    public Student()
+    {
+      Count++;
+    }
+    public Student(string name, int grade, string birthday, string address, int phone)
+    {
+      Name = name;
+      Grade = grade;
+      Birthday = birthday;
+      Address = address;
+      Phone = phone;
+
+      Count++;
     }
 
-    class Student
+    public int Phone
     {
-        static public int Count;
-        public string Name;
-        public int Grade;
-        public string Birthday;
-        public string Address;
-        private int phone;
-
-        public Student()
-        {
-            Count++;
-        }
-        public Student(string name, int grade, string birthday, string address, int phone)
-        {
-            Name = name;
-            Grade = grade;
-            Birthday = birthday;
-            Address = address;
-            Phone = phone;
-
-            Count++;
-        }
-
-        public  int Phone
-        {
-            set { phone = value;}
-        }
-
-        public int getPhone() {
-            return phone;
-        }
+      set { phone = value; }
     }
 
-    class Teacher
+    public int getPhone()
     {
-        static public int Count;
-
-        public string Name;
-
-        public string Subject;
-
-        private int phone;
-
-        public Teacher() {
-            Count++;
-        }
-
-        public Teacher(string name, string subject, int phone) {
-            Name = name;
-            Subject = subject;
-            Phone = phone;
-
-            Count++;
-        }
-
-        public int Phone
-        {
-            set { phone = value; }
-        }
-
-        public int getPhone()
-        {
-            return phone;
-        }
+      return phone;
     }
+
+    public void Display()
+    {
+      Console.WriteLine($"Name: {Name}");
+      Console.WriteLine($"Grade: {Grade}");
+      Console.WriteLine($"Birthday: {Birthday}");
+    }
+  }
+
+  class Teacher : Member
+  {
+    static public int Count;
+    public string Subject;
+
+    public Teacher()
+    {
+      Count++;
+    }
+
+    public Teacher(string name, string subject, int phone)
+    {
+      Name = name;
+      Subject = subject;
+      Phone = phone;
+
+      Count++;
+    }
+
+    public int Phone
+    {
+      set { phone = value; }
+    }
+
+    public int getPhone()
+    {
+      return phone;
+    }
+
+
+    public void Display()
+    {
+      Console.WriteLine($"Name: {Name}");
+      Console.WriteLine($"Subject: {Subject}");
+      Console.WriteLine($"Phone: {getPhone()}");
+    }
+  }
 }
